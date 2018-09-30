94b44a4ace5c3bb7157ebd31863a9b75182fb6ac
Lrnc_Olg <laurence@example.com>
2018-09-30 11:19:11 +0200

    Change List into ArrayList

--- TD1Sorciers/src/com/company/Main.java
+++ TD1Sorciers/src/com/company/Main.java
@@ -1,28 +1,36 @@
 package com.company;
 
-/*import java.util.*;
-AnayList<Personnage> l = new AnayList<Personnage>();
+import java.util.*;
+/*
+ArrayList<Personnage> l = new AnayList<Personnage>();
 l.add(new Montre (...));
 l.get(3);
  */
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        /*Personnage p1;
+        /*
+        Personnage p1;
         p1 = new Monstre("Dracula",32);
         Personnage p2;
         p2 = new Sorcier("Panoramix",45);
 
         p1.afficher();
         p2.afficher();
-
         */
+        List<String> nomMonstre = Arrays.asList("Dracula", "Frankeinstein", "Boo", "Fantôme", "Loup");
+        List<String> nomSorcier = Arrays.asList("Panoramix", "Druide", "Harry Potter", "Dumbledore", "Baltazar");
+
+         /*
         String nomMonstre[] = {"Dracula", "Frankeinstein", "Boo", "Fantôme", "Loup"};
         String nomSorcier[] = {"Panoramix", "Druide", "Harry Potter", "Dumbledore", "Baltazar"};
+        */
 
-
-        Personnage tableau[] = new Personnage[10];
+        List<Personnage> tableau = new ArrayList<>();
+        //Personnage tableau[] = new Personnage[10];
         int j = 0;
         for (int i = 0; i < 10; i++) {
             //char n = (char) (i + 65);
@@ -30,17 +38,17 @@ public class Main {
             double pv = Math.random() * 100;
             int total = (int) pv;
             if (j < 4) {
-                String nom = nomSorcier[i];
-                tableau[i] = new Sorcier(nom, total);
+                String nom = nomSorcier.get(i);
+                tableau.add(new Sorcier(nom, total));
             } else if (j==4) {
                 String nom = "Magicien";
-                tableau[i] = new Magicien(nom, total);
+                tableau.add(new Magicien(nom, total));
             } else {
-                String nom = nomMonstre[i - 5];
-                tableau[j] = new Monstre(nom, total);
+                String nom = nomMonstre.get(i - 5);
+                tableau.add(new Monstre(nom, total));
             }
             j++;
-            tableau[i].afficher();
+            tableau.get(i).afficher();
 
         }
 
@@ -54,8 +62,8 @@ public class Main {
                 double nu2 = Math.random() * 10;
                 num2 = (int) nu2;
             }
-            while (num != num2 & tableau[num2].mort());
-            tableau[num].attaque(tableau[num2]);
+            while (num != num2 & tableau.get(num2).mort());
+            tableau.get(num).attaque(tableau.get(num2));
         }
 
     }
